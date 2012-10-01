6b80fead211e15782cf2087d32dd344f8fe379ed
Hyunsung John Kim <hyjkim@soe.ucsc.edu>
2012-10-01 12:09:36 -0700

    Made a management command for the importation of clonotype datasets

---
+++ clonotypes/management/commands/import_tsv.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from clonotypes.models import Clonotype
+from samples.models import Sample
+
+
+class Command(BaseCommand):
+  args = '<sample_id tsv_filename>'
+  help = 'Imports a TSV file for a predefined patient'
+
+  def handle(self, *args, **options):
+    try:
+      sample_id, tsv_filename = args
+#    self.stdout.write(" ".join(args))
+      self.stdout.write( sample_id + " " + tsv_filename)
+    except:
+      raise BaseException("Incorrect number of arguments for import")
+
+    try:
+      sample = Sample.objects.get(id=sample_id)
+    except:
+      raise BaseException("Sample with sample id " + sample_id + " not found.")
+
+    Clonotype.import_tsv(sample, tsv_filename)
+
+
+
