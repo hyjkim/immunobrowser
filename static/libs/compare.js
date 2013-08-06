8fb6ff857344891e090c5f5d7ab7b5a2f961d522
hyjkim <hyjkim@gmail.com>
2013-08-05 23:48:55 -0700

    added j histogram and too dark gridlines.

--- static/libs/compare.js
+++ static/libs/compare.js
@@ -1,5 +1,5 @@
 function scatterNav() {
-  var margin = {top: 50, right: 50, bottom: 50, left: 50},
+  var margin = {top: 50, right: 10, bottom: 50, left: 60},
       width=600,
       height = 400,
       xValue = function(d) {return d[0]},
@@ -10,8 +10,8 @@ function scatterNav() {
       colorScale = d3.scale.category10(),
       nameMap = function(d) {return d[3]},
       vHeight = 100,
-      jWidth = 100;
-
+      jWidth = 100,
+      sampleIds = [];
 
   function nav (selection) {
     selection.each(function(data) {
@@ -31,6 +31,9 @@ function scatterNav() {
       .key(function(d) {return d[0]})
       .sortKeys(d3.ascending)
       .entries(data);
+    
+    // set the sample Ids
+     sampleIds = nestedData.map(function(d) {return d.key});
 
     var svg = d3.select(this).selectAll("svg").data([nestedData]);
 
@@ -81,7 +84,8 @@ function scatterNav() {
     var xAxis = d3.svg.axis()
       .scale(xScale)
       .orient("bottom")
-      .ticks(xScale.range().length);
+      .ticks(xScale.range().length)
+      .tickSize(-height+margin.top+margin.bottom,0,0);
 
     gEnter.append("g")
       .attr("class", "axis xAxis")
@@ -91,11 +95,17 @@ function scatterNav() {
     var yAxis = d3.svg.axis()
       .scale(yScale)
       .orient("left")
-      .ticks(yScale.range().length);
+      .ticks(yScale.range().length)
+      .tickSize(-width,0,0)
+      ;
 
     gEnter.append("g")
       .attr("class", "axis yAxis")
       .call(yAxis);
+
+    // draw a grid
+    
+    
 
     // set up tooltips
     // generate the tooltip div
@@ -210,18 +220,29 @@ function scatterNav() {
     });
 
 
+    // J histogram 
+    var jHistSvg = selection.append("svg")
+      .attr("class", "hist-j")
+      .attr("height", height)
+      .attr("width", jWidth);
+
+    var jHistInner = jHistSvg.append("g").attr("class", "inner")
+      .attr("transform", 
+          "translate(0,"+margin.top+")")
+      .datum(data);
+
+
     // V histogram
-    // this should probably be a function to facillitate updates
 
     // Make the svg that holds the plot
     var vHistSvg = selection.append("svg")
-      .attr("class", "v-hist")
+      .attr("class", "hist-v")
       .attr("height", vHeight)
       .attr("width", width);
 
     var vHistInner = vHistSvg.append("g").attr("class", "inner")
       .attr("transform", 
-          "translate(" + margin.left + ",20)")
+          "translate(" + margin.left + ",10)")
       .datum(data);
 
 
@@ -233,10 +254,16 @@ function scatterNav() {
         var vGene = d3.select(this).datum();
         d3.select(this).attr("fill", "blue");
         d3.selectAll("g.v-" +vGene+ " circle").attr("class", "active");
+
+      var filteredTestData = data.filter(function (d) {
+        return d[0] == vGene
+      });
+      jHistInner.datum(filteredTestData).call(jHist);
       })
     .on("mouseout", function () {
       xLabels.attr("fill", null);
       sampleCircles.attr("class", "inactive");
+//      jHistInner.datum(data).call(jHist);
     });
 
     // highlight circles when y-axis tick labels are highlighted
@@ -249,13 +276,12 @@ function scatterNav() {
         .filter(function(d) {return d[1] == jGene})
         .attr("class", "active");
 
-      // Try with a filtered dataset
+      // Redraw v histograms with a filtered dataset
       var filteredTestData = data.filter(function (d) {
         return d[1] == jGene
       });
 
       vHistInner.datum(filteredTestData).call(vHist);
-
       })
     .on("mouseout", function () {
       xLabels.attr("fill", null);
@@ -264,12 +290,124 @@ function scatterNav() {
 //      vHistInner.datum(data).call(vHist);
     });
 
+    jHistInner.call(jHist);
     vHistInner.call(vHist);
-
-    // J histogram
 
     // slider?
     });
+  }
+
+  // j hist plot
+  function jHist(selection) {
+    var jNest = d3.nest()
+      .key(function(d) {return d[3]})
+      .key(function(d) { return d[1] })
+      .rollup(function(leaves) {
+        return d3.sum(leaves, function (d){
+          return d[2]
+        })
+      })
+    .entries(selection.datum());
+
+    var jSeries = makeSeries(jNest, yScale);
+
+    // calculate the scale
+    var jHistScale = histScale(jNest)
+      .range([0, jWidth]);
+
+    var jLine = d3.svg.line()
+      .x(function(d) {return jHistScale(d.freq)})
+      .y(function(d) {return yScale(d.group)});
+
+    // Make the actual lines
+    var jPath = selection.selectAll(".line")
+      .data(jSeries,
+          function(d) {return d.key} 
+          );
+
+      jPath.exit().remove();
+
+      jPath
+      .enter()
+      .append("path")
+      .attr("class", "line")
+      .attr("stroke-width", 1)
+      .attr("stroke", function(d) {return colorScale(d.key)})
+      .attr("fill", "none");
+
+    selection.selectAll(".line")
+      .attr("d", function(d) {return jLine(d.values)});
+
+    var jG = selection.selectAll("g.points")
+      .data(jSeries,
+          function(d) {return d.key}
+          );
+
+    jG.enter()
+      .append("g")
+      .attr("class", function(d,i) {
+        return "sample-" + i + " points";
+      })
+    .attr("fill", function(d) {return colorScale(d.key)})
+
+      var jPoints = jG
+      .selectAll("circle")
+      .data(function(d) {return d.values},
+          function(d) {return [d.group, d.sample]}
+          );
+
+    // Delete old circles
+    jPoints.exit().remove();
+
+    // Create new circles
+    jPoints
+      .enter()
+      .append("circle")
+      .attr("r", 5)
+      .attr("class", "inactive");
+
+    // Plot the circles
+    selection.selectAll("circle")
+      .attr("cy", function(d) { return yScale(d.group)})
+      .attr("cx", function(d,i ) { return jHistScale(d.freq)});
+
+    // Plot the axis
+    var xAxis = d3.svg.axis()
+      .scale(jHistScale)
+      .orient("bottom")
+      .ticks(3);
+
+    var gAxis = selection.select("g.xAxis");
+    if (gAxis.empty()) {
+      gAxis = selection
+        .append("g")
+        .attr("class", "axis xAxis");
+    }
+    gAxis.call(xAxis);
+
+    gAxis  .attr("transform", "translate(0," + (d3.max(yScale.range()) + ")"))
+
+    // Add some tooltips
+    jPoints
+      .on("mouseover", function()  {
+        d3.select(this).attr("class", "active");
+        //        tooltip.style("visibility", "visibile");
+        //        tooltip.text("test");
+      })
+    .on("mousemove", function()  {
+      //      return tooltip.style("top",
+      //        (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+    })
+    .on("mouseout", function()  {
+      jPoints.attr("class", "inactive");
+      //      tooltip.html("");
+      //      tooltip.style("visibility", "hidden");
+    })
+    .on("click", function(d) {
+      alert(d.key + " " + d.values);
+    });
+
+
   }
 
   // v hist plot
@@ -285,81 +423,55 @@ function scatterNav() {
         })
       })
     .entries(selection.datum());
-
-    // Make the series
-    var vSeriesOld = vNest.map(function(d) {
-      return d.values;
-    });
-
-    var vNestHash = vNest.map(function(sampleData) {
-      var tmp = {};
-      sampleData.values.forEach(function(d) {
-        tmp[d.key] = d.values;
-      });
-      return tmp;
-    });
-
-    var vSeries = vNest.map(function(sampleData, i) {
-      var sampleDataMap = d3.map(vNest);
-      return xScale.domain().map(function (d) {
-        if (d in vNestHash[i]) {
-          return {key: d, values: vNestHash[i][d]};
-        }
-        else {
-          return {key: d, values: 0};
-        }
-      })
-    });
-
-    console.log(vSeriesOld);
+    var vSeries = makeSeries(vNest, xScale);
     console.log(vSeries);
-    console.log(vNestHash);
 
     // calculate the scale
-    var vHistScale = d3.scale
-      .linear()
-      .domain([0, d3.max(vNest, function(sample) {
-        return d3.max(sample.values, function (d) {
-          return d.values;
-        })
-      })
-          ])
+    var vHistScale = histScale(vNest)
       .range([0, vHeight-margin.bottom]);
 
     var vLine = d3.svg.line()
-      .x(function(d) {return xScale(d.key)})
-      .y(function(d,i) {console.log("Line: "+d.key + " " + d.values); return vHistScale(d.values)});
+      .x(function(d) {return xScale(d.group)})
+      .y(function(d) {
+        return vHistScale(d.freq)});
 
     // Make the actual lines
-    selection.selectAll(".line")
-      .data(vSeries)
-      .enter()
+    var vPath = selection.selectAll(".line")
+      .data(vSeries);
+
+    vPath.exit().remove();
+
+      vPath.enter()
       .append("path")
       .attr("class", "line")
       .attr("stroke-width", 1)
-      .attr("stroke", function(d,i) {return colorScale(i)})
-      //        .attr("stroke", "black")
+      .attr("stroke", function(d) {return colorScale(d.key)})
       .attr("fill", "none");
 
-      // Update the lines
-      selection.selectAll(".line")
-      .attr("d", vLine);
+    // Update the lines
+    selection.selectAll(".line")
+      .attr("d", function(d) {return vLine(d.values)});
 
     // Add some points
-    var vG= selection.selectAll("g.points")
-      .data(vSeries);
+    var vG = selection.selectAll("g.points")
+      .data(vSeries,
+          function(d) {return d.key});
 
-      vG
-      .enter()
+    vG.exit().remove();
+
+    vG.enter()
       .append("g")
       .attr("class", function(d,i) {
         return "sample-" + i + " points";
       })
-    .attr("fill", function(d,i) {return colorScale(i)})
+    .attr("fill", function(d) {return colorScale(d.key)})
 
       var vPoints = vG
       .selectAll("circle")
-      .data(function(d) {return d});
+      .data(
+          function(d) {return d.values},
+          function(d) {return [d.group, d.sample]}
+          );
 
     // Delete old circles
     vPoints.exit().remove();
@@ -373,8 +485,9 @@ function scatterNav() {
 
     // Plot the circles
     selection.selectAll("circle")
-      .attr("cx", function(d) { return xScale(d.key)})
-      .attr("cy", function(d,i ) { console.log("Points: "+d.key + " " + d.values); return vHistScale(d.values)});
+      .attr("cx", function(d) { return xScale(d.group)})
+      .attr("cy", function(d,i ) { return vHistScale(d.freq)});
+
 
     // Plot the axis
     var yAxis = d3.svg.axis()
@@ -385,38 +498,82 @@ function scatterNav() {
     var gAxis = selection.select("g.yAxis");
     if (gAxis.empty()) {
       gAxis = selection
-      .append("g")
-      .attr("class", "axis yAxis");
+        .append("g")
+        .attr("class", "axis yAxis");
     }
-      gAxis.call(yAxis);
-
-
+    gAxis.call(yAxis);
 
     // Add some tooltips
     vPoints
       .on("mouseover", function()  {
         d3.select(this).attr("class", "active");
-//        tooltip.style("visibility", "visibile");
-//        tooltip.text("test");
+        //        tooltip.style("visibility", "visibile");
+        //        tooltip.text("test");
       })
     .on("mousemove", function()  {
-//      return tooltip.style("top",
-//        (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+      //      return tooltip.style("top",
+      //        (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
     })
     .on("mouseout", function()  {
       vPoints.attr("class", "inactive");
-//      tooltip.html("");
-//      tooltip.style("visibility", "hidden");
+      //      tooltip.html("");
+      //      tooltip.style("visibility", "hidden");
     })
     .on("click", function(d) {
       alert(d.key + " " + d.values);
     });
 
     // and some tooltips
+  }
 
+  function histScale(nest) {
+    return d3.scale
+      .linear()
+      .domain([0, d3.max(nest, function(sample) {
+        return d3.max(sample.values, function (d) {
+          return d.values;
+        })
+      })
+          ])
 
   }
 
+  function makeSeries(nest, scale) {
+    var hash = nest.map(function(sampleData) {
+      var tmp = {};
+      sampleData.values.forEach(function(d) {
+        tmp[d.key] = d.values;
+      });
+      return tmp;
+    });
+
+    return nest.map(function(sampleData, i) {
+      var sampleDataMap = d3.map(nest);
+      return {
+        key: sampleData.key,
+           values: scale.domain().map(function (d) {
+             if (d in hash[i]) {
+               return {
+                 group: d,
+           freq: hash[i][d],
+           sample: sampleData.key
+               };
+             }
+             else {
+               return {
+                 group: d, 
+           freq: 0, 
+           sample: sampleData.key
+               };
+             }
+           }
+           )}
+    });
+  }
+
+  /*
+   * Public Accessor functions
+   */
 
   // The x-accessor for the circle generator
   function X(d) {
