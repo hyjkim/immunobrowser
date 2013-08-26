84ebbba302ca82808bdf2ed0004a60c68afc8ec7
hyjkim <hyjkim@gmail.com>
2013-08-26 16:58:01 -0700

    Changed the default site url in the settings

--- immuno/settings.py
+++ immuno/settings.py
@@ -40,7 +40,7 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
+SITE_ID = laozi.soe.ucsc.edu:8000
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
